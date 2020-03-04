3e8fe314ca5bb2abe553b6b36ae625e08f799e4d
Jack <jackvarney@live.com>
2020-03-04 23:52:54 +0000

    Typed advertising package value

--- src/core/constants/advertising-package-values.ts
+++ src/core/constants/advertising-package-values.ts
@@ -1,6 +1,6 @@
 import { AdvertisingPackage } from "../types/AdvertisingPackage";
 
-export const advertisingPackageValue = {
+export const advertisingPackageValue: Record<AdvertisingPackage, number> = {
   [AdvertisingPackage.none]: 0,
   [AdvertisingPackage.basic]: 500,
   [AdvertisingPackage.advanced]: 5000
